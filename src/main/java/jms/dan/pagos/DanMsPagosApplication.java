202664a6765544bfe03dac99c1a3762fd98938ba
MalenaMoix <moixmalena@gmail.com>
2021-06-07 20:15:28 -0300

    Initial commit

---
+++ src/main/java/jms/dan/pagos/DanMsPagosApplication.java
@@ -0,0 +1,13 @@
+package jms.dan.pagos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsPagosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanMsPagosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jms/dan/pagos/DanMsPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package jms.dan.pagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsPagosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
